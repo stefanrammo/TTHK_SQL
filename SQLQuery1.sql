a5502c88b069097bab8e58577d18cc710e83a59c
StefanRammo <stefanrammo85@gmail.com>
2024-02-26 13:40:27 +0200

    init

---
+++ SQLQuery1.sql
@@ -0,0 +1,24 @@
+--loome db
+create database TTHK_SQL
+
+--db valimine
+use TTHK_SQL	
+
+--db kustutamine
+drop database TTHK_SQL
+
+--tabeli loomine
+create table Gender
+(
+Id int not null primary key,
+Gender nvarchar(10) not null
+)
+
+--andmete sisestamine
+insert into Gender (Id, Gender)
+values (1, 'Male'),
+(2, 'Female'),
+(3, 'Unknown')
+
+--vaatame tabeli sisu
+select * from Gender
