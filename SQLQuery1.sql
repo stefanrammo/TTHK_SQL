0d67c9bce1152ea4f899564ef896ebc3b7ca3c34
StefanRammo <stefanrammo85@gmail.com>
2024-02-26 15:52:08 +0200

    Continue 1

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -22,3 +22,130 @@ values (1, 'Male'),
 
 --vaatame tabeli sisu
 select * from Gender
+
+--loome uue tabeli
+create table Person
+(
+Id int not null primary key,
+Name nvarchar(30),
+Email nvarchar(30),
+GenderId int 
+)
+
+--andmete sisestamine
+insert into Person (Id, Name, Email, GenderId) values
+(1, 'Superman', 's@s.com', 1),
+(2, 'Wonderwoman', 'w@w.com', 2),
+(3, 'Batman', 'b@b.com', 1),
+(4, 'Aquaman', 'a@a.com', 1),
+(5, 'Catwoman', 'c@c.com', 2),
+(6, 'Antman', 'ant"ant.com', 1),
+(7, NULL, NULL, 3),
+(8, 'Spiderman', 's@s.com', NULL)
+
+--vaata tabel Person andmeid
+select * from Person
+
+--võõrvõtme ühenduse loomine kahe tabeli vahel
+alter table Person add constraint tblPerson_GenderId_FK
+foreign key (GenderId) references Gender(Id)
+
+--kui sisetad uue rea andmeid ja ei ole sisestanud GenderId-d,
+--siis see automaatselt sisestatakse sellele reale väärtusega 3 (unknown)
+alter table Person
+add constraint DF_persons_GenderId
+default 3 for GenderId
+
+--sisestame andmed default väärtusega GenderId's
+insert into Person (Id, Name, Email) values
+(9, 'Ironman', 'i@i.com')
+
+--lisame uue veeru
+alter table Person
+add Age nvarchar(10)
+
+--lisame nr piirangu vanuse sisestamisel
+alter table Person
+add constraint CK_Person_Age check (Age > 0 and Age < 155)
+
+--sisestame uue rea andmeid 
+insert into Person (Id, Name, Email, GenderId, Age) values
+(10, 'Kalevipoeg', 'k@k.com', 1, 154)
+
+--muudame andmeid koodiga
+update Person
+set Age = 35
+where Id = 9
+
+select * from Person
+
+--sisestame muutuja City nvarchar(50)
+alter table Person
+add City nvarchar(50)
+
+--sisestame andmed City veergu
+update Person
+set City = 'Kaljuvald'
+where Id = 10
+
+select * from Person
+
+--kõik kes elavad Gotham linnas
+select * from Person where City = 'Gotham'
+--kõik kes ei ela Gothamis
+select * from Person where City != 'Gotham'
+--kõik kes ei ela Gothamis
+select * from Person where City <> 'Gotham'
+--kõik kes ei ela Gothamis
+select * from Person where not City = 'Gotham'
+
+--näitab teatud vanusega inimesi
+select * from Person where Age = 120 or Age = 35 or Age = 19
+--näitab teatud vanusega inimesi
+select * from Person where Age in (120, 35, 19)
+
+--näitab teatud vanuse vahemikus inimesi (kaasa arvatud)
+select * from Person where Age between 18 and 31
+
+--wildcard ehk näitab kõik g-tähega algavad linnad
+select * from Person where City like 'g%'
+--kõik emailid kus on @ märk sees
+select * from Person where Email like '%@%'
+
+--näitab emaile kus ei ole @ sees
+select * from Person where Email not like '%@%'
+
+--näitab kellel on emailis ees ja peale @ märki ainult üks täht
+select * from Person where Email like '_@_.com'
+
+--näitab 3 märki enne ja peale @ emailis
+update Person
+set Email = 'bat@bat.com'
+where Id = 3
+select * from Person where Email like '___@___.com'
+
+--kõik kellel nimes ei ole esimene täht w, a, c
+select * from Person where Name like '[^WAC]%'
+
+--kes elavad Gothamis ja New Yorkis
+select * from Person where City = 'Gotham' or City = 'New York'
+
+--kõik kes elavad välja toodud linnades ja on vanemad kui 29
+select * from Person where City = 'Gotham' or City = 'New York' and Age > 29
+
+--kuvab tähestikulises järjekorras inimesi ja võtab aluseks nime
+select * from Person order by Name
+--kuvab vastupidi
+select * from Person order by Name DESC
+
+--võtab kolm esimest rida
+select top 3 * from Person
+
+--võtab kolm esimest rida, veergudega Age ja Name
+select top 3 Age, Name from Person
+
+--näita esimene 50% tabelist
+select top 50 percent * from Person
+
+--järjesta vanuse järgi isikud
+select * from Person order by cast(Age as int)
